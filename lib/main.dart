3588e8dddab5bcf2007d78565cc8a58c44c74383
allymreid <amackenzier@mun.ca>
2025-06-01 16:38:36 -0230

    Feature: App Routing with GoRouter

--- lib/main.dart
+++ lib/main.dart
@@ -2,41 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:hive/hive.dart';
-
-part 'main.g.dart';
-
-@HiveType(typeId: 0)
-class Expense extends HiveObject {
-  @HiveField(0)
-  String name;
-
-  @HiveField(1)
-  double amount;
-
-  Expense({required this.name, required this.amount});
-}
+import 'package:hive_flutter/hive_flutter.dart';
+import 'models/expense.dart';
+import 'routing/app_router.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   final appDir = await getApplicationDocumentsDirectory();
-  Hive.init(appDir.path);
+  await Hive.initFlutter(appDir.path);
   Hive.registerAdapter(ExpenseAdapter());
   await Hive.openBox<Expense>('expenses');
-  runApp(ProviderScope(child: MainApp()));
-}
 
+  runApp(const ProviderScope(child: MainApp()));
+}
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp.router(
+      routerConfig: appRouter,
+      debugShowCheckedModeBanner: false,
     );
   }
 }
