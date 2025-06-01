2c2c169f0839918a1c2bcd06423000b91ab8b99e
allymreid <amackenzier@mun.ca>
2025-06-01 16:32:30 -0230

    Feature: Hive Setup and Expense Model

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:hive/hive.dart';
 
-void main() {
-  runApp(const MainApp());
+part 'main.g.dart';
+
+@HiveType(typeId: 0)
+class Expense extends HiveObject {
+  @HiveField(0)
+  String name;
+
+  @HiveField(1)
+  double amount;
+
+  Expense({required this.name, required this.amount});
 }
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final appDir = await getApplicationDocumentsDirectory();
+  Hive.init(appDir.path);
+  Hive.registerAdapter(ExpenseAdapter());
+  await Hive.openBox<Expense>('expenses');
+  runApp(ProviderScope(child: MainApp()));
+}
+
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
